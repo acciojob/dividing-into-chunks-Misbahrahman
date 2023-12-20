fb31c8419214e8ae51859f971b4fc701ef4e4a74
Misbahur rahman <98620184+Misbahrahman@users.noreply.github.com>
2023-12-20 12:14:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+	
+  let ans = [];
+  let sum = 0;
+  let a = [];
+  for(let i of arr){
+	  
+	  sum += i;
+	  a.push(i);
+
+	  if(sum > n){
+		  a.pop();
+		  ans.push(a);
+		  a = [];
+          a.push(i);
+		  sum = i;
+	  }
+  }	
+
+  if(a.length != 0)ans.push(a);
+
+	return ans;
 };
 
 const n = prompt("Enter n: ");
